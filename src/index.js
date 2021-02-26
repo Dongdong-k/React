d59b9f6d0e423196165df05986e83f25848e0921
dongdong <crisish@naver.com>
2021-02-26 18:43:41 +0900

    1. useState

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App(){
+  // [state, setState] = useState
+  //  state = 변수, setState = 콜백함수로 state를 가지고 있는 함수
+  const [state, setState] = useState(1)
+  const incrementItem = () => setState(state +1 )
+  const decrementItem = () => setState(state -1 )
+  const doubleItem = () => setState(state*2 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start coding</h2>
+      <h2>Current State : {state}</h2>
+      <button onClick={incrementItem}>Increase</button>
+      <button onClick={decrementItem}>Decrease</button>
+      <button onClick={doubleItem}>Double Times</button>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
