8a9014b1de81c6d450da091101ca293a75ff7e4d
dongdong <crisish@naver.com>
2021-03-02 20:42:28 +0900

    #2.1 useTitle

--- src/index.js
+++ src/index.js
@@ -2,19 +2,22 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function App() {
-  const [number, setNumber] = useState(0);
-  const [anumber, setaNumber] = useState(0);
-  const sayhello = () => console.log("number가 변경 되었습니다");
-  // useEffect : componentWillUpdate, DidMount 등과 동일한 기능 수행
-  // [] 안에 있는 내용이 변경될 때, function-sayhello 기능 수행
-  useEffect(sayhello, [number]);
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
 
+function App() {
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("home"), 5000);
   return (
     <div className="App">
       <h1>Hi</h1>
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => setaNumber(anumber + 1)}>{anumber}</button>
     </div>
   );
 }
