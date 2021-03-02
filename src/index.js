896c7e398c457f75a591811325e9cbfaf4569044
dongdong <crisish@naver.com>
2021-03-02 20:17:53 +0900

    #2.0 Introduction to useEffect

--- src/index.js
+++ src/index.js
@@ -1,51 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// API 나 다른 곳으로부터 정보를 가져올 때
-const content = [
-  {
-    tab: "Section 1",
-    content: "I'm the contecnt of the Section 1",
-  },
-  {
-    tab: "Section 2",
-    content: "I'm the contecnt of the Section 2",
-  },
-];
-
-const useTab = (initialTab, allTabs) => {
-  const [currentIndex, setCurrentIndex] = useState(initialTab);
-  // setCurrentIndex(currentIndex);
-  // 오류 점검하기 - allTabs 미입력 or Array 형태가 아닌 경우
-  if (!allTabs || !Array.isArray(allTabs)) {
-    console.log("allTabs 입력 되지 않았습니다 or Array 아닙니다");
-    console.log(!allTabs);
-    console.log(!Array.isArray(allTabs));
-    return;
-  } else {
-    console.log("오류 없음");
-  }
-  return {
-    currentItem: allTabs[currentIndex],
-    changeItem: setCurrentIndex,
-  };
-};
-
 function App() {
-  const { currentItem, changeItem } = useTab(0, content);
-  const section1 = () => changeItem(0);
-  const section2 = () => changeItem(1);
-  // console.log(currentItem.content);
+  const [number, setNumber] = useState(0);
+  const [anumber, setaNumber] = useState(0);
+  const sayhello = () => console.log("number가 변경 되었습니다");
+  // useEffect : componentWillUpdate, DidMount 등과 동일한 기능 수행
+  // [] 안에 있는 내용이 변경될 때, function-sayhello 기능 수행
+  useEffect(sayhello, [number]);
+
   return (
     <div className="App">
-      {content.map((sectionName, index) => (
-        <button onClick={() => changeItem(index)}>{sectionName.tab}</button>
-      ))}
-      <button onClick={section1}>section1</button>
-      <button onClick={section2}>section2</button>
-      <div>{currentItem.content}</div>
-      {/* <button onClick={currentItem.section2}>section2</button> */}
+      <h1>Hi</h1>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setaNumber(anumber + 1)}>{anumber}</button>
     </div>
   );
 }
