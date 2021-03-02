8faab1be3bb0aa0bf361c2d5714631b03e4049d3
dongdong <crisish@naver.com>
2021-03-02 21:17:11 +0900

    useClick

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function App() {
-  const focusInput = useRef();
-  setTimeout(() => {
-    console.log(focusInput);
-    console.log(focusInput.current);
-    focusInput.current.focus();
-  }, 2000);
+// reference 사용하여 선택하고 싶은 요소 연결
+// 연결한 요소를 useEffect로 연결하여 실행하고 싶은 함수 매칭
+const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    element.current.addEventListener("click", onClick);
+  }, []);
+  return element;
+};
 
+function App() {
+  const sayhello = () => console.log("hello");
+  const title = useClick(sayhello);
   return (
     <div className="App">
-      <h1>Hi</h1>
-      <input ref={focusInput} placeholder="hi" />
+      <h1 ref={title}>Hi</h1>
     </div>
   );
 }
