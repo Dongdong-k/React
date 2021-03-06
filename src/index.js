354edc1dde816d3fbf256663fed9c99e6e01e1b7
dongdong <crisish@naver.com>
2021-03-06 16:07:14 +0900

    useConfirm 정의

--- src/index.js
+++ src/index.js
@@ -2,25 +2,40 @@ import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// reference 사용하여 선택하고 싶은 요소 연결
-// 연결한 요소를 useEffect로 연결하여 실행하고 싶은 함수 매칭
-const useClick = (onClick) => {
-  const element = useRef();
-  useEffect(() => {
-    element.current.addEventListener("click", onClick);
-  }, []);
-  return element;
+// 기능 : 저장하거나 삭제 등 기능을 실행할 때 다시 한번 확인하고 입력 결과값에 따라 기능 수행
+const useConfirm = (message = " ", callback, rejection) => {
+  if (typeof callback !== "function") {
+    return;
+  }
+  const confirmAction = () => {
+    // confirm : 메세지를 띄우는 창으로 사용시 window.confirm 으로 입력 필요
+    if (window.confirm(message)) {
+      callback();
+    } else {
+      rejection();
+    }
+  };
+  return confirmAction;
 };
 
-function App() {
-  const sayhello = () => console.log("hello");
-  const title = useClick(sayhello);
+const App = () => {
+  // yes_function 정의
+  const deleteWorldFunction = () => console.log("Deleted the World");
+  // no_function 정의
+  const deleteWorldRejection = () => console.log("Rejected");
+  // useConfirm(초기값, yes_func, no_func) 기능 할당 및 button onclick 연결하기
+  const deleteWorldAction = useConfirm(
+    "Are you sure?",
+    deleteWorldFunction,
+    deleteWorldRejection
+  );
   return (
     <div className="App">
-      <h1 ref={title}>Hi</h1>
+      <h1>Hi</h1>
+      <button onClick={deleteWorldAction}>Delete the world</button>
     </div>
   );
-}
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
