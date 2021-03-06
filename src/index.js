22326efa4b8231c7fbdd13648a4c4c3c36fb327a
dongdong <crisish@naver.com>
2021-03-06 17:09:42 +0900

    useBeforLeave

--- src/index.js
+++ src/index.js
@@ -2,30 +2,32 @@ import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// 기능 : 홈페이지 종료시 종료할 것인지 묻는 경고창 활성화
-const usePreventLeave = () => {
-  // event 발생시 실행 기능 : beforeunload 기능 사용시 아래 내용 작성 필요
-  const listener = (event) => {
-    event.preventDefault();
-    event.returnValue = "";
+// 기능 : 마우스의 위치에 따라서 원하는 function 기능 수행
+// 여기서는 창 상단 위로 마우스가 이탈시 console.log 실행
+const useBeforeLeave = (onBefore) => {
+  // 이탈시 원하는 동작 정의 : 창 상단으로 이탈시 console.log
+  const leaveAction = (event) => {
+    console.log(event);
+    const { clientY } = event;
+    if (clientY <= 0) {
+      onBefore();
+    }
   };
-  // function 작성하여 eventlistener 로 연결하고 추후 function 기능을 반환
-  const enablePrevent = () => window.addEventListener("beforeunload", listener);
-  const disablePrevent = () =>
-    window.removeEventListener("beforeunload", listener);
-
-  return { enablePrevent, disablePrevent };
+  // useEffect 기능 사용하여 해당 이벤트 발생시 내용 업데이트
+  useEffect(() => {
+    document.addEventListener("mouseleave", leaveAction);
+    return () => document.removeEventListener("mouseleave", leaveAction);
+  }, []);
 };
 
 const App = () => {
-  // usePreventLeave 에서 반환된 함수의 기능을 할당 받고 버튼에 연결
-  const { enablePrevent, disablePrevent } = usePreventLeave();
-
+  // 이벤트 발생시 실행하기 원하는 기능 함수로 정의
+  const begforyou = () => console.log("Do you really want to leave?");
+  // useBeforeLeave 에 함수 할당 및 기능 활성화
+  useBeforeLeave(begforyou);
   return (
     <div className="App">
       <h1>Hi</h1>
-      <button onClick={enablePrevent}>Protect</button>
-      <button onClick={disablePrevent}>UnProtect</button>
     </div>
   );
 };
