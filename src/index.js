5726a67f733e1f3fc6c24f950b922437a1c57528
dongdong <crisish@naver.com>
2021-03-06 19:02:22 +0900

    useFadeIn & useNetwork

--- src/index.js
+++ src/index.js
@@ -2,32 +2,54 @@ import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// 기능 : 마우스의 위치에 따라서 원하는 function 기능 수행
-// 여기서는 창 상단 위로 마우스가 이탈시 console.log 실행
-const useBeforeLeave = (onBefore) => {
-  // 이탈시 원하는 동작 정의 : 창 상단으로 이탈시 console.log
-  const leaveAction = (event) => {
-    console.log(event);
-    const { clientY } = event;
-    if (clientY <= 0) {
-      onBefore();
-    }
-  };
-  // useEffect 기능 사용하여 해당 이벤트 발생시 내용 업데이트
+const useFadeIn = (duration = 1, color = "blue") => {
+  const element = useRef();
   useEffect(() => {
-    document.addEventListener("mouseleave", leaveAction);
-    return () => document.removeEventListener("mouseleave", leaveAction);
+    if (element.current) {
+      console.log(typeof element.current);
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s, color ${duration}s`;
+      current.style.color = color;
+      current.style.opacity = 1;
+    }
   }, []);
+  return { ref: element, style: { opacity: 0 } };
+};
+
+const useNetwork = (onChange) => {
+  const [status, setStatus] = useState(navigator.onLine);
+
+  const handler = () => {
+    if (typeof onChange === "function") {
+      onChange(navigator.onLine);
+    }
+    setStatus(navigator.onLine);
+  };
+  useEffect(() => {
+    window.addEventListener("online", handler);
+    window.addEventListener("offline", handler);
+
+    return () => {
+      window.removeEventListener("online", handler);
+      window.removeEventListener("offline", handler);
+    };
+  }, []);
+  return status;
 };
 
 const App = () => {
-  // 이벤트 발생시 실행하기 원하는 기능 함수로 정의
-  const begforyou = () => console.log("Do you really want to leave?");
-  // useBeforeLeave 에 함수 할당 및 기능 활성화
-  useBeforeLeave(begforyou);
+  const handleonLine = (online) => {
+    console.log(online ? "we are Online" : "we are offline");
+  };
+  const onLine = useNetwork(handleonLine);
+  const fadeInh1 = useFadeIn(5, "red");
+  const fadeInp = useFadeIn(1);
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <h1 {...fadeInh1}>Hi</h1>
+      <p {...fadeInp}>hihihihihihihihi</p>
+      <h1>{onLine ? "OnLine" : "OffLine"}</h1>
+      <h1>{onLine ? "True" : "False"}</h1>
     </div>
   );
 };
