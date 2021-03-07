f1eba19e51d77bc83a68047501ca72f5fcdeb22c
dongdong <crisish@naver.com>
2021-03-07 17:13:30 +0900

    useFullscreen

--- src/index.js
+++ src/index.js
@@ -2,28 +2,46 @@ import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const useScroll = () => {
-  const [state, setState] = useState({ x: 0, y: 0 });
+// 버튼을 누르면 전체화면 활성화
+// 전체화면을 시킬 이미지 useRef로 연결
+// useRef 내부에 선택된 이미지를 전체화면으로 만드는 함수 설정
+// 버튼 - onclick 와 활성화 시키는 함수와 연결
 
-  const onScroll = () => {
-    console.log("x : ", window.scrollX, "y : ", window.scrollY);
-    setState({ x: window.scrollX, y: window.scrollY });
+const useFullscreen = (callback) => {
+  const element = useRef();
+  const triggerFullscreen = () => {
+    if (element.current) {
+      element.current.requestFullscreen();
+      callback(true);
+    }
+  };
+  const triggerCloseFullscreen = () => {
+    callback(false);
+    document.exitFullscreen();
   };
 
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-    return () => {
-      window.addEventListener("scroll", onScroll);
-    };
-  }, []);
-  return state;
+  return { element, triggerFullscreen, triggerCloseFullscreen };
 };
 
+// 전체화면 끄기 버튼을 보기 위해 그림을 전체화면 시키지 않고 div를 연결하여 전체화면 시켜줌
 const App = () => {
-  const { y } = useScroll();
+  const checkFullscreen = (isFull) => {
+    console.log(isFull ? "we are full" : "we are small");
+  };
+  const { element, triggerFullscreen, triggerCloseFullscreen } = useFullscreen(
+    checkFullscreen
+  );
   return (
     <div className="App" style={{ height: "1000vh" }}>
-      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>Hi</h1>
+      <div ref={element}>
+        <img
+          src="https://images.freecreatives.com/wp-content/uploads/2015/10/moon-landscape-dream-wallpapers.jpg"
+          width="50%"
+          alt="Moon"
+        />
+        <button onClick={triggerCloseFullscreen}>CloseFullscreen</button>
+      </div>
+      <button onClick={triggerFullscreen}>Fullscreen</button>
     </div>
   );
 };
