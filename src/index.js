c351ccb92f11cb07791f2625a1da19f705f0c4f6
dongdong <crisish@naver.com>
2021-03-07 17:33:59 +0900

    useNotification

--- src/index.js
+++ src/index.js
@@ -2,46 +2,39 @@ import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// 버튼을 누르면 전체화면 활성화
-// 전체화면을 시킬 이미지 useRef로 연결
-// useRef 내부에 선택된 이미지를 전체화면으로 만드는 함수 설정
-// 버튼 - onclick 와 활성화 시키는 함수와 연결
+// notification API 사용하여 알람 띄우기 기능 만들기
+// 버튼을 만들고 알람 기능 활성화시키기
+// 현재 알림 허가여부 확인하고 허가시 알람 띄우기
+// window에 notification 기능이 없으면 종료
 
-const useFullscreen = (callback) => {
-  const element = useRef();
-  const triggerFullscreen = () => {
-    if (element.current) {
-      element.current.requestFullscreen();
-      callback(true);
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
+  }
+  const checkNotification = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then(() => {
+        if (Notification.permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
     }
   };
-  const triggerCloseFullscreen = () => {
-    callback(false);
-    document.exitFullscreen();
-  };
 
-  return { element, triggerFullscreen, triggerCloseFullscreen };
+  return checkNotification;
 };
 
-// 전체화면 끄기 버튼을 보기 위해 그림을 전체화면 시키지 않고 div를 연결하여 전체화면 시켜줌
 const App = () => {
-  const checkFullscreen = (isFull) => {
-    console.log(isFull ? "we are full" : "we are small");
-  };
-  const { element, triggerFullscreen, triggerCloseFullscreen } = useFullscreen(
-    checkFullscreen
-  );
+  const checkNotification = useNotification("Do you like coffee?", {
+    body: "I love coke",
+  });
   return (
     <div className="App" style={{ height: "1000vh" }}>
-      <div ref={element}>
-        <img
-          src="https://images.freecreatives.com/wp-content/uploads/2015/10/moon-landscape-dream-wallpapers.jpg"
-          width="50%"
-          alt="Moon"
-        />
-        <button onClick={triggerCloseFullscreen}>CloseFullscreen</button>
-      </div>
-      <button onClick={triggerFullscreen}>Fullscreen</button>
+      <button onClick={checkNotification}>Notification</button>
     </div>
   );
 };
